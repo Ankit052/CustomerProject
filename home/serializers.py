5efb7a06c983fb3a84e7d4ea7b54d7f1fa3973e0
Ankit <ankitjha1099@gmail.com>
2022-02-17 21:51:06 +0530

    added  serializers

--- home/serializers.py
+++ home/serializers.py
@@ -11,6 +11,25 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["first_name","last_name","email","mobile_no"]
 
 class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta :
         model = Customer
         fields = ["profile_number","user"]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(
+            first_name = user_data['first_name'],
+            last_name = user_data['last_name'],
+            email = user_data['email'],
+            mobile_no = user_data['mobile_no']
+        )
+        
+        # import pdb; pdb.set_trace()
+        print(user_data)
+        customer = Customer.objects.create(
+            profile_number = validated_data['profile_number'],
+            user= user
+        )
+
+        return customer   
